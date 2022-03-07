59c79a10c8ae039ff9c0b0354bf6e73dfec859c6
DaltonHart <daltonvaren@gmail.com>
2022-03-07 14:03:19 -0800

    :tada: (base) base setup with app requirements

---
+++ app.js
@@ -0,0 +1,23 @@
+/**
+ * TODO
+ * React History
+ * React vs ReactDOM
+ * == Components == 
+ * - types 
+ *    -- functional 
+ *    -- class based
+ * - data within components 
+ *    -- props 
+ *    -- state
+ * - roles 
+ *    -- container 
+ *    -- computational 
+ *    -- presentational
+ *    -- page   
+ */
+
+/* === Functional === */
+
+/* === Class Based === */
+
+/* === ReactDOM === */
