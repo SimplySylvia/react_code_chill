e3d57a8fb799b2cf3e8a0f14e56ac2a890f07bec
DaltonHart <daltonvaren@gmail.com>
2022-03-09 10:11:59 -0800

    :pencil: (components ) adds breakdown of components

--- notes.js
+++ notes.js
@@ -18,5 +18,12 @@
  *    -- Reusable
  *    -- Small 
  *    -- Testable   
- * 
+ */
+
+/** 
+ * What are we building? 
+ * Let's build a quick page based off of something fun like twitch.tv! 
+ *  We will run through some of the base functionality of the channel page so we can gain an understanding of the different organization of components available
+ * For a breakdown of the components check out the component.drawio file
+ * You will need to install the drawio extension to do so.
  */
