f6fad80fdec875b65c7ea435e8c090a79463bac8
DaltonHart <daltonvaren@gmail.com>
2022-03-09 09:25:58 -0800

    :recycle: (refactored for setup of future session)

---
+++ README.md
@@ -0,0 +1,5 @@
+# React Playground for Code and Chills 
+
+[Here](https://github.com/DaltonHart/HowTo-React-Webpack-Babel) is a link to a repo with the code and setup process for starting an application from scratch. 
+
+You can also find the directions in this repo under [HOWTO.md](./HOWTO.md)
---
+++ notes.js
@@ -0,0 +1,22 @@
+/**
+ * TODO
+ * == Components == 
+ * - types (Code style)
+ *    -- functional -> lightweight returns react elements 
+ *    -- class based -> heavier returns react elements with lifecycle methods (State)
+ * - data within components 
+ *    -- props 
+ *    -- state
+ * - roles 
+ *    -- container 
+ *    -- computational 
+ *    -- presentational
+ *    -- page
+ * - Ideology F.I.R.S.T. 
+ *    -- Focused
+ *    -- Independent 
+ *    -- Reusable
+ *    -- Small 
+ *    -- Testable   
+ * 
+ */
